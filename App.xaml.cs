12f2a4299b13b1e34ad87b6297764e51b13fe3ae
Gebruiker <Gebruiker@DESKTOP-S5LJ6C1>
2025-03-26 15:39:08 +0100

    ok

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,8 +5,28 @@
         public App()
         {
             InitializeComponent();
+        }
 
-            MainPage = new AppShell();
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+
+        // Optional events to implement for the application lifecycle. The Window created 
+        // above also has lifecycle events that can be used to track the Window lifecycle.
+        protected override void OnStart()
+        {
+            base.OnStart();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+        }
+
+        protected override void OnSleep()
+        {
+            base.OnSleep();
         }
     }
 }
